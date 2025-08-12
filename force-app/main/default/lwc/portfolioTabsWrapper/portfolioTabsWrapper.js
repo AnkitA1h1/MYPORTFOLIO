b4fdff635ab14b1868836a31d801b85ec1a8bb7b
Kumar <ankit.eh.kumar@accenture.com>
2025-08-12 16:42:21 +0530

    commit Portfolio tab summary

---
+++ force-app/main/default/lwc/portfolioTabsWrapper/portfolioTabsWrapper.js
@@ -0,0 +1,7 @@
+import { api, LightningElement } from 'lwc';
+
+export default class PortfolioTabsWrapper extends LightningElement {
+    @api recordId //= 'a01gK00000Fb507QAB'
+
+    @api objectApiName //= 'Portfolio__c'
+}
